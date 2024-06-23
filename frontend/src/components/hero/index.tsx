daa9d1f12015c24c1572eb73d962c67be9c77286
Anandhu Prakash <anandhuprakash682@gmail.com>
2024-06-23 14:34:34 +0530

    (started, hero) started hero section

--- frontend/src/components/hero/index.tsx
+++ frontend/src/components/hero/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-
-type Props = {}
+import React from "react";
+import "./hero.scss";
+type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
       <section className="hero">
-          
+          <div></div>
+      <div className="hero_container">
+        <div className="hero_text">
+          <h2>New Arraival!</h2>
+          <h1>Discover Our New Collection</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            inventore ex voluptate deleniti nostrum quod illo perferendis!
+            Harum, maxime expedita!
+          </p>
+        </div>
+        <div className="hero_button">
+          <a href="/shop" className="button">
+            Buy Now
+          </a>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import HeroSection from "../../components/hero";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Home;
